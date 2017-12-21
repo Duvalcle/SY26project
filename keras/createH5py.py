c58131a62888a5f3af69457f23283323adeb6843
Alexis CARE <alx.care@gmail.com>
2017-12-21 16:38:59 +0100

    cleaning & comment

--- keras/createH5py.py
+++ keras/createH5py.py
@@ -10,44 +10,51 @@ import h5py
 import cv2
 import matplotlib.pyplot as plt
 
+#taille de l'image en entrée
 IMG_SIZE_HEIGHT = 24
 IMG_SIZE_WIDHT = 32
+#association classe numéro
 CLASSES = {"Carre": 0, "Cercle": 1, "Losange": 2, "Croix": 3, "Triangle": 4, "Octogone": 5}
 NUM_CLASSES = len(CLASSES)
 
+#fonction pour labéliser l'image en fonction du chemin
 def get_class(img_path):
     classe = img_path.split('/')[-2]
     return CLASSES[classe]
 
 path = sys.argv[1]
-shuffle_data = True  # shuffle the addresses before saving
-hdf5_path = 'dataset.hdf5'  # address to where you want to save the hdf5 file
+shuffle_data = True
+hdf5_path = 'dataset.hdf5'
+#récupération de tout les chemin vers les images
 train_addrs = glob.glob(os.path.join(path, '*/*.*'))
 train_shape = (len(train_addrs), IMG_SIZE_HEIGHT, IMG_SIZE_WIDHT, 3)
 
+# mélange des chemins
 if shuffle_data:
     shuffle(train_addrs)
+# labélisation
 train_labels = [get_class(addr) for addr in train_addrs]
 
+#création du fichier hdf5
 hdf5_file = h5py.File(hdf5_path, mode='w')
 hdf5_file.create_dataset("size_image", [3], np.int16)
 hdf5_file.create_dataset("train_img", train_shape, np.float32)
-# hdf5_file.create_dataset("train_mean", train_shape[1:], np.float32)
 hdf5_file.create_dataset("train_labels", (len(train_addrs),), np.int8)
+# écriture des label et la taille de l'entré du réseau de neurone
 hdf5_file["train_labels"][...] = train_labels
 hdf5_file["size_image"][...] = train_shape[1:4]
 
-
-mean = np.zeros(train_shape[1:], np.float32)
+# pour tout les chemins
 for i in range(len(train_addrs)):
+    # lecrure de l'image
     addr = train_addrs[i]
     img = cv2.imread(addr)
+    # resize de l'image
     img = cv2.resize(img, (IMG_SIZE_WIDHT, IMG_SIZE_HEIGHT), interpolation=cv2.INTER_CUBIC)
-    #img = (img / 127.5) -1.
     if i % 100 == 0 and i > 1:
         print 'Train data: {}/{}'.format(i, len(train_addrs))
+    # ecriture dans le fichier de l'image
     hdf5_file["train_img"][i, ...] = img[None]
-    mean += img / float(len(train_labels))
 
-# hdf5_file["train_mean"][...] = mean
+#fermeture du fichier
 hdf5_file.close()
--- keras/train.py
+++ keras/train.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
-# np.random.seed(123)  # for reproducibility
 import os
 import sys
 import cv2
-    # import tensorflow as tf
-import csv
 import glob
 import h5py
 
@@ -15,26 +12,18 @@ from keras.utils import HDF5Matrix
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D, AveragePooling2D
-from keras.optimizers import SGD, Adam
-from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 from keras import backend as K
-# K.set_image_data_format('channels_first')
 
-from keras.datasets import mnist
 
 # IMG_SIZE_HEIGHT = 96
 # IMG_SIZE_WIDHT = 128
 CLASSES = {"Carre": 0, "Cercle": 1, "Losange": 2, "Croix": 3, "Triangle": 4, "Octogone": 5}
 NUM_CLASSES = len(CLASSES)
 
-def lr_schedule(epoch):
-    return lr * (0.1 ** int(epoch / 10))
-
-def get_class(img_path):
-    classe = img_path.split('/')[-2]
-    return CLASSES[classe]
-
+# fonction pour définir le réseau de neurone
 def cnn_model(input_shape):
     model = Sequential()
 
@@ -45,9 +34,6 @@ def cnn_model(input_shape):
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2))) #Test Clement
     model.add(Conv2D(64, (3,3), activation='relu'))
-    #model.add(AveragePooling2D(pool_size=(4, 4), strides=(4,4))) #Test Clement
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Dropout(0.25))
     model.add(Flatten())
@@ -68,28 +54,38 @@ def main(arg):
     imgs = []
     labels = []
 
+    # récuparition des données du fichier
     x_train = HDF5Matrix(path, 'train_img')
     y_train = HDF5Matrix(path, 'train_labels')
     input_shape = HDF5Matrix(path, 'size_image')
+
+    #debuf print
     print "X shape:", x_train.shape
     print "Y shape:", y_train.shape
     print "input_shape:", input_shape[0]
     print "input_shape:", input_shape[1]
     print "input_shape:", input_shape[2]
 
+    # transtypage
     input_shape = np.array(input_shape, dtype='int')
-
     y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
+
+    # création du réseau de neurone
     model = cnn_model(input_shape)
+
+    # règle d'aprentissage Adam
     sgd = Adam(lr=0.001)
-    # sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+
+    # compilation du modéle
     model.compile(loss='categorical_crossentropy',
                   optimizer=sgd,
                   metrics=['accuracy'])
 
+    # parametre d'apprentissage
     batch_size = 30
     epochs = 20
 
+    # entrainement du model
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
