4faa89d601559a2d0c2c7bdbaf3bb77eaac1157c
Alexis CARE <alx.care@gmail.com>
2017-12-21 14:51:22 +0100

    cleaning and comment

--- camera_computer/gabor/camera.cpp
+++ camera_computer/gabor/camera.cpp
@@ -6,16 +6,69 @@
 using namespace std;
 using namespace cv;
 
+//fonction pour troncer les valeurs des photos entre 0 et 1
 void troncate(Mat &img) {
 	threshold(img, img, 1, 1, THRESH_TRUNC);
 	threshold(img, img, 0, 0, THRESH_TOZERO);
 }
 
+//fonction pour normalizer l'image
 void regulate(Mat &img) {
 	double min, max;
 	minMaxLoc(img, &min, &max);
 	img = (img - min) / (max - min);
-	cout << "\nmin : " << min << "\t max : " << max << endl;
+}
+
+void detecte_edges_gabor(Mat & img) {
+	// definition des kernels
+	int kernel_size = 7;
+	double sig = 2.5, lm = 6, gm = 0.3, ps = 0;
+
+	double theta[4];
+	theta[0] = 0 ;
+	theta[1] = CV_PI/4 ;
+	theta[2] = CV_PI/2 ;
+	theta[3] = 3 * CV_PI/4 ;
+
+  Mat kernel[4], prKer[4];
+  for(int i =0; i<4; i++){
+		kernel[i] =getGaborKernel(Size(kernel_size,kernel_size), sig, theta[i], lm,gm, ps, CV_32F);
+		resize(kernel[i], prKer[i], cv::Size(), 10, 10);
+		regulate(prKer[i]);
+	}
+	Mat temp[4];
+	Mat result;
+
+	//couleur en niveaux de gris
+  cvtColor(img,img,COLOR_BGR2GRAY);
+
+	//detection de contours
+  for(int i =0; i<4; i++){
+		filter2D(img, temp[i], CV_32F, kernel[i]);
+		troncate(temp[i]);
+	}
+
+	bitwise_and(temp[0], temp[1], img);
+	bitwise_and(img, temp[2], img);
+	bitwise_and(img, temp[3], img);
+
+	//afficher les kernels
+	// imshow("kernel0", prKer[0]);
+	// imshow("kernel1", prKer[1]);
+	// imshow("kernel2", prKer[2]);
+	// imshow("kernel3", prKer[3]);
+
+	//afficher les detections de contours
+	// imshow("camera0", temp[0]);
+	// imshow("camera1", temp[1]);
+	// imshow("camera3", temp[3]);
+	// imshow("camera2", temp[2]);
+
+	//afficher les detections de contours au carré
+	// imshow("camera0_2", temp_2[0]);
+	// imshow("camera2_2", temp_2[2]);
+	// imshow("camera1_2", temp_2[1]);
+	// imshow("camera3_2", temp_2[3]);
 }
 
 int main(int narg, char *argv[]) {
@@ -34,85 +87,22 @@ int main(int narg, char *argv[]) {
 		cerr<<"Error opening the camera"<<endl;
 		return -1;
 }
-int kernel_size = 7;
-double sig = 2.5, lm = 6, gm = 0.3, ps = 0;
-
-	double theta[4];
-	//theta[0] = 180;
-	//theta[1] = 225;
-	//theta[2] = 270;
-	//theta[3] = 135;
-
-	theta[0] = 0 ;
-	theta[1] = CV_PI/4 ;
-	theta[2] = CV_PI/2 ;
-	theta[3] = 3 * CV_PI/4 ;
 
     Mat frame;
-
     cout << "on filme" << endl;
-
-    Mat kernel[4], prKer[4];
-    for(int i =0; i<4; i++){
-			kernel[i] = getGaborKernel(Size(kernel_size,kernel_size), sig, theta[i], lm,gm, ps, CV_32F);
-			// cout << kernel[i] << "\n" << endl;
-			// if (i == 0 or i == 2){
-			// 	kernel[i] -= 0.5;
-			// }
-			resize(kernel[i], prKer[i], cv::Size(), 10, 10);
-			regulate(prKer[i]);
-			// double min, max;
-			// minMaxLoc(prKer[i], &min, &max);
-			// cout << "min : " << min << "\t max : " << max << endl;
-			// prKer[i] = (prKer[i] - min) / (max - min);
-	  }
 
     for(;;) {
 		Camera.grab();
-		//Mat sobx_2, soby_2, sobx, soby, result;
-		Mat temp[4];
-		Mat result;
     Camera.retrieve ( frame);
+
+		//afficher la photo initiale
 		imshow("camera init", frame);
 
-    cvtColor(frame,frame,COLOR_BGR2GRAY); //oui ou non ???
-    // equalizeHist( frame, frame );
+		//detections de contours
+		detecte_edges_gabor(frame);
 
-    //Sobel(frame, sobx, CV_8U, 1, 0);
-    //Sobel(frame, soby, CV_8U, 0, 1);
-    //pow(sobx, 2.0, sobx_2);
-    //pow(soby_2, 2.0, soby_2);
-    //sqrt(sobx + sobx_2, result);
-    //result = sobx + soby;
-
-    for(int i =0; i<4; i++){
-			filter2D(frame, temp[i], CV_32F, kernel[i]);
-			troncate(temp[i]);
-		}
-
-		Mat temp_2[4];
-
-		 for(int i =0; i<4; i++){
-			pow(temp[i], 2.0,  temp_2[i]);
-		}
-		imshow("camera0", temp[0]);
-		imshow("camera2", temp[2]);
-		imshow("camera1", temp[1]);
-		imshow("camera3", temp[3]);
-		//
-    // imshow("camera0_2", temp_2[0]);
-    // imshow("camera2_2", temp_2[2]);
-    // imshow("camera1_2", temp_2[1]);
-    // imshow("camera3_2", temp_2[3]);
-
-    bitwise_and(temp_2[0], temp_2[1], result);
-    bitwise_and(result, temp_2[2], result);
-    bitwise_and(result, temp_2[3], result);
-
-
-    imshow("frame", frame);
-
-		imshow("camera", result);
+		//afficher la détéction de contours
+		imshow("camera", frame);
 
         if(waitKey(10) >= 0) break;
     }
--- camera_computer/laplace/camera_Laplace.cpp
+++ camera_computer/laplace/camera_Laplace.cpp
@@ -6,28 +6,37 @@
 using namespace std;
 using namespace cv;
 
-void detecte_edges(Mat & img) {
+//fonction de detection de contours par un kernel de laplace
+void detecte_edges_laplace(Mat & img) {
 	Mat ep2, ep3;
 
+	//definition du paramettre du filtre de laplace
 	int ddepth = CV_8U, ksize = 3;
 	double scale = 0.1, delta = 0;
 
+	//couleur en niveaux de gris
 	cvtColor(img,img,COLOR_BGR2GRAY);
 
 	double minVal, maxVal;
 
+	//reduction du bruit par plusieus filtre gaussien
 	GaussianBlur(img,img, Size(7,7), 1, 1);
 	GaussianBlur(img,img, Size(7,7), 1, 1);
 	GaussianBlur(img,img, Size(7,7), 1, 1);
 
+	//filtre de laplace
 	Laplacian(img, ep2, ddepth, ksize, scale, delta, BORDER_DEFAULT );
 
+	//normalisation de l'image
 	minMaxLoc(ep2, &minVal, &maxVal); //find minimum and maximum intensities
 	ep2.convertTo(ep3, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
+
+	//réduction du bruit de detection
 	for(int i = 0; i < 2; i++){
 		medianBlur(ep3,ep3, 3);
 	}
 
+	//un seuil adaptatif pour souligné les contours
 	adaptiveThreshold(ep3, img, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 0);
 }
 
@@ -55,10 +64,13 @@ int main(int narg, char *argv[]) {
     for(;;) {
 		Camera.grab();
     Camera.retrieve ( frame);
+		//afficher la photo initiale
 		imshow("camera init", frame);
 
+		//detections de contours
 		detecte_edges(frame);
 
+		//afficher la détéction de contours
     imshow("frame", frame);
 
         if(waitKey(10) >= 0) break;
